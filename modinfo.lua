6284131b81773582b8b6c93a118a012d73bc773e
Caio Mathielo <mathielo@gmail.com>
2017-12-08 21:47:51 +0200

    Set proper compatibility and dummy icon

--- modinfo.lua
+++ modinfo.lua
@@ -8,7 +8,7 @@ description = "Makes it possible to disable the walk by clicking action in the g
 author = "machete_kills"
 
 --A version number so you can ask people if they are running an old version of your mod.
-version = "0.0.1"
+version = "0.1.0"
 
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 6
@@ -16,9 +16,9 @@ api_version_dst = 10
 priority = 0
 
 --Compatible with both the base game and Reign of Giants
-dont_starve_compatible = true
-reign_of_giants_compatible = true
-shipwrecked_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
 dst_compatible = true
 
 --This lets clients know if they need to get the mod from the Steam Workshop to join the game
@@ -30,8 +30,8 @@ client_only_mod = true
 --This lets people search for servers with this mod by these tags
 server_filter_tags = {}
 
-icon_atlas = ""
-icon = ""
+icon_atlas = "toggle-c2w.tex"
+icon = "toggle-c2w.tex"
 
 forumthread = ""
 
