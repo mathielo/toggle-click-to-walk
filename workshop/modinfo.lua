df600bbf122aabbb1fa2028ed674accd20f3710c
Caio Mathielo <mathielo@gmail.com>
2017-12-09 18:52:10 +0100

    Enable Dont Starve / RoG / SW compatibility

--- workshop/modinfo.lua
+++ workshop/modinfo.lua
@@ -8,7 +8,7 @@ description = "Makes it possible to disable the walk by clicking action in the g
 author = "machete_kills"
 
 --A version number so you can ask people if they are running an old version of your mod.
-version = "0.1.2"
+version = "0.2.0"
 
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 6
@@ -16,9 +16,9 @@ api_version_dst = 10
 priority = 0
 
 --Compatible with both the base game and Reign of Giants
-dont_starve_compatible = false
-reign_of_giants_compatible = false
-shipwrecked_compatible = false
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
 dst_compatible = true
 
 --This lets clients know if they need to get the mod from the Steam Workshop to join the game
