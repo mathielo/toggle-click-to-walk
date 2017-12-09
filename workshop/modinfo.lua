140b8f9128d1c443ede903153d875d807e200479
Caio Mathielo <mathielo@gmail.com>
2017-12-09 18:09:52 +0100

    Add TEX icon and atlas

--- workshop/modinfo.lua
+++ workshop/modinfo.lua
@@ -8,7 +8,7 @@ description = "Makes it possible to disable the walk by clicking action in the g
 author = "machete_kills"
 
 --A version number so you can ask people if they are running an old version of your mod.
-version = "0.1.1"
+version = "0.1.2"
 
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 6
@@ -30,8 +30,8 @@ client_only_mod = true
 --This lets people search for servers with this mod by these tags
 server_filter_tags = {}
 
-icon_atlas = ""
-icon = ""
+icon_atlas = "toggle-c2w.xml"
+icon = "toggle-c2w.tex"
 
 forumthread = "/files/file/1788-toggle-click-to-walk/"
 
